6d250c126d25a4fa7fb128172e2499fc8a0355bb
Cristi <dumi_cristi@yahoo.com>
2020-11-18 19:41:52 +0200

    Creare clase de baza si citire mesaj de la tastatura

--- Project1/Project1/Header.h
+++ Project1/Project1/Header.h
@@ -1,1 +1,41 @@
 #pragma once
+
+#include<iostream>
+
+using namespace std;
+
+
+enum TypeCommand{
+	CREATE, DROP, DYSPLAY , INSERT, SELECT, UPDATE, DELETE
+};
+
+class Command {
+private:
+	TypeCommand typeCommand;
+	string tableName;
+
+public:
+
+
+
+
+};
+
+
+enum TypeCollumn{
+	text, integer, intreg
+};
+class Table {
+private:
+	int collumn[9999];
+	int row[9999];
+	TypeCollumn typeCollumn[9999];
+
+public:
+
+
+
+
+
+
+};
--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,8 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
 
 
-int main()
-{
+int main(){
+	//declarare si citire comanda luata de la tastatura
+	char comandaDeLaStdin[100];
+	char* p=0;//declarare pointer pt strtok, pentru a putea sparge sirul in subsiruri
+	cout << "Dati comanda:";
+	cin.getline(comandaDeLaStdin, sizeof(comandaDeLaStdin));//citire cu spatii de la stdin
+	p=strtok(comandaDeLaStdin, " ");//salvam primul subsir(Numele comenzii in comandaDeLaStdin)
+
+	//mergem prin restul sirurilor
+	while (p != NULL) {
+		printf("%s\n", p);
+
+		p = strtok(NULL, " ");
+	}
+
+
+
+
+
 	return 1;
 }
