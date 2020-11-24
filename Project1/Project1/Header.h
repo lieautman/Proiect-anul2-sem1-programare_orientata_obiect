1378006985c6eebbbcaddba0d23c73db5b2f6e6d
Cristi <dumi_cristi@yahoo.com>
2020-11-24 23:19:47 +0200

    commit 21/11/2020

--- Project1/Project1/Header.h
+++ Project1/Project1/Header.h
@@ -4,38 +4,131 @@
 
 using namespace std;
 
+enum TypeCommand {
+	Create, Drop, Display, Insert, Delete, Select, Update
+};
 
-enum TypeCommand{
-	CREATE, DROP, DYSPLAY , INSERT, SELECT, UPDATE, DELETE
+class CommandTypeAtributes {
+private:
+	TypeCommand typeCommand;
+
+public:
 };
 
 class Command {
 private:
-	TypeCommand typeCommand;
 	string tableName;
-
+	char* firstWord;
+	char* secondWord;
+	bool isOk;
+	CommandTypeAtributes  commandTypeAtributes;
 public:
+#pragma region Constructor simplist
+	Command(){
+		this->tableName = " ";
+		this->firstWord = 0;
+		this->isOk = 1;
+	}
+#pragma endregion Constructor simplist
 
+	void setFirstWord(char*p) {
+		if (p != NULL) {
+			//Sql is not case sensitive
+			for(int i=0;i<strlen(p)+1;i++)
+			p[i]=toupper(p[i]);
+			//We need buffer to save values we search for
+			char* buffer[7];
+			#pragma region initializare buffer vector
+			buffer[0] = new char[strlen("CREATE") + 1];
+			strcpy(buffer[0], "CREATE");
+			buffer[1] = new char[strlen("DROP") + 1];
+			strcpy(buffer[1], "DROP");
+			buffer[2] = new char[strlen("DYSPLAY") + 1];
+			strcpy(buffer[2], "DYSPLAY");
+			buffer[3] = new char[strlen("INSERT") + 1];
+			strcpy(buffer[3], "INSERT");
+			buffer[4] = new char[strlen("SELECT") + 1];
+			strcpy(buffer[4], "SELECT");
+			buffer[5] = new char[strlen("UPDATE") + 1];
+			strcpy(buffer[5], "UPDATE");
+			buffer[6] = new char[strlen("DELETE") + 1];
+			strcpy(buffer[6], "DELETE");
+			#pragma endregion initializare buffer vector
+			#pragma region initializare atribut cuvant current
+			if (strcmp(buffer[0], p) == 0)
+				this->firstWord = p;
+			else if(strcmp(buffer[1], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[2], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[3], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[4], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[5], p) == 0)
+				this->firstWord = p;
+			else {
+				char* buffer2;
+				buffer2 = new char[strlen("GRESIT") + 1];
+				strcpy(buffer2, "GRESIT");
+				this->firstWord = buffer2;
+				this->isOk = 0;
+			}
+			#pragma endregion initializare atribut cuvant current
+		}
+	}
+	char* getFirstWord() {
+		return this->firstWord;
+	}
 
-
-
-};
-
-
-enum TypeCollumn{
-	text, integer, intreg
-};
-class Table {
-private:
-	int collumn[9999];
-	int row[9999];
-	TypeCollumn typeCollumn[9999];
-
-public:
-
-
-
-
-
-
+	//nu este ok
+	void setSecondWord(char* p) {
+		if (p != NULL) {
+			//Sql is not case sensitive
+			for (int i = 0; i < strlen(p) + 1; i++)
+				p[i] = toupper(p[i]);
+			//We need buffer to save values we search for
+			char* buffer[7];
+#pragma region initializare buffer vector
+			buffer[0] = new char[strlen("CREATE") + 1];
+			strcpy(buffer[0], "CREATE");
+			buffer[1] = new char[strlen("DROP") + 1];
+			strcpy(buffer[1], "DROP");
+			buffer[2] = new char[strlen("DYSPLAY") + 1];
+			strcpy(buffer[2], "DYSPLAY");
+			buffer[3] = new char[strlen("INSERT") + 1];
+			strcpy(buffer[3], "INSERT");
+			buffer[4] = new char[strlen("SELECT") + 1];
+			strcpy(buffer[4], "SELECT");
+			buffer[5] = new char[strlen("UPDATE") + 1];
+			strcpy(buffer[5], "UPDATE");
+			buffer[6] = new char[strlen("DELETE") + 1];
+			strcpy(buffer[6], "DELETE");
+#pragma endregion initializare buffer vector
+#pragma region initializare atribut cuvant current
+			if (strcmp(buffer[0], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[1], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[2], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[3], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[4], p) == 0)
+				this->firstWord = p;
+			else if (strcmp(buffer[5], p) == 0)
+				this->firstWord = p;
+			else {
+				char* buffer2;
+				buffer2 = new char[strlen("GRESIT") + 1];
+				strcpy(buffer2, "GRESIT");
+				this->firstWord = buffer2;
+				this->isOk = 0;
+			}
+#pragma endregion initializare atribut cuvant current
+		}
+	}//nu este ok
+	char* getSecondWord() {
+		return this->secondWord;
+	}
 };
--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -10,23 +10,50 @@ int main(){
 	char* p=0;//declarare pointer pt strtok, pentru a putea sparge sirul in subsiruri
 	cout << "Dati comanda:";
 	cin.getline(comandaDeLaStdin, sizeof(comandaDeLaStdin));//citire cu spatii de la stdin
-	p=strtok(comandaDeLaStdin, " ");//salvam primul subsir(Numele comenzii in comandaDeLaStdin)
-	//Cream obiectul de tip comanda
-	Command comanda;
 	
+	//Cream obiectul de tip comanda
+	Command command;
 
+	//Luam primul cuvand si vedem ce tip de comanda vom folosi
+	p=strtok(comandaDeLaStdin, " ");//salvam primul subsir(Numele comenzii in comandaDeLaStdin)
+	command.setFirstWord(p);
+	printf("%s\n", command.getFirstWord());
 
+	//Facem switch in functie de primul cuvant
+	char* buffer0;
+	buffer0 = command.getFirstWord();
+	char* buffer[7];
+	#pragma region initializare buffer vector
+		buffer[0] = new char[strlen("CREATE") + 1];
+		strcpy(buffer[0], "CREATE");
+		buffer[1] = new char[strlen("DROP") + 1];
+		strcpy(buffer[1], "DROP");
+		buffer[2] = new char[strlen("DYSPLAY") + 1];
+		strcpy(buffer[2], "DYSPLAY");
+		buffer[3] = new char[strlen("INSERT") + 1];
+		strcpy(buffer[3], "INSERT");
+		buffer[4] = new char[strlen("SELECT") + 1];
+		strcpy(buffer[4], "SELECT");
+		buffer[5] = new char[strlen("UPDATE") + 1];
+		strcpy(buffer[5], "UPDATE");
+		buffer[6] = new char[strlen("DELETE") + 1];
+		strcpy(buffer[6], "DELETE");
+	#pragma endregion initializare buffer vector
 
-	//mergem prin restul sirurilor
-	while (p != NULL) {
-		printf("%s\n", p);
-
+	//Luam cel de-al doilea cuvant si vedem daca se incadreaza in ce trebuie
+	if (p != NULL) {
 		p = strtok(NULL, " ");
+		command.setSecondWord(p);
+		printf("%s\n", command.getSecondWord());
+
+
+		//mergem prin restul sirurilor
+		while (p != NULL) {
+			printf("%s\n", command.getFirstWord());
+			p = strtok(NULL, " ");
+			command.setFirstWord(p);
+		}
 	}
-
-
-
-
 
 	return 1;
 }
